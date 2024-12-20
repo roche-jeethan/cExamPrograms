c094adc0b4ea3a094718e03f4ea6ed0c5376b3b0
Jeethan Roche <jeethanroche@gmail.com>
2024-12-20 20:40:07 +0530

    Added Ackermann's function

---
+++ M2/ackermanRecursive.c
@@ -0,0 +1,20 @@
+//recursive ackermann function
+#include<stdio.h>
+#include<stdlib.h>
+int ackerman(int m, int n){
+    if(m==0){
+        return n+1;
+    } else if(m>0 && n==0){
+        return ackerman(m-1,1);
+    } else if(m>0 && n>0){
+        return ackerman(m-1,ackerman(m,n-1));
+    }
+}
+int main(){
+    int m,n;
+    printf("Enter the parameters for the ackermann\'s function: ");
+    scanf("%d %d",&m,&n);
+    int res = ackerman(m,n);
+    printf("The result is %d\n",res);
+    return 0;
+}
