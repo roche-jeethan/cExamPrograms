34318c80d7499e70c87e304fc9f4a654c8d976dd
Jeethan Roche <jeethanroche@gmail.com>
2024-12-20 19:48:50 +0530

    updated GCD program

--- M2/gcdRecursive.c
+++ M2/gcdRecursive.c
@@ -1,12 +1,5 @@
 /*gcd programs with recursion function.*/
 #include <stdio.h>
-int factorial(int n)
-{
-    if (n == 0)
-        return 1;
-    else
-        return n * factorial(n - 1);
-}
 int gcd(int a, int b)
 {
     if (b != 0)
@@ -16,10 +9,7 @@ int gcd(int a, int b)
 }
 int main()
 {
-    int n, a, b;
-    printf("Enter the number for factorial: ");
-    scanf("%d", &n);
-    printf("Factorial of %d is %d\n", n, factorial(n));
+    int a, b;
     printf("Enter two numbers for gcd: ");
     scanf("%d %d", &a, &b);
     printf("GCD of %d and %d is %d\n", a, b, gcd(a, b));
